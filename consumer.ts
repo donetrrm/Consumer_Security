ddf4fba4e4d602c1a337cb7ea8fd5e164f2af4a2
Iskander <iskanderramos8@gmail.com>
2024-04-19 00:25:29 -0600

    reportes moded

--- consumer.ts
+++ consumer.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+let IPEsp32 = "";
+
 async function consumeMessages() {
 
 const options = {
@@ -14,13 +16,30 @@ const options = {
 
 const  url = process.env.AMQP_URL || "";
  const queue = process.env.AMQP_QUEUE || ""
-
+ const IPQueue = "esp32";
   const connection = await amqp.connect(url, options);
   const channel = await connection.createChannel();
+  const channelIP = await connection.createChannel();
 
+  await channelIP.assertQueue(IPQueue, { durable: true });
   await channel.assertQueue(queue, { durable: true });
 
-  console.log(`Escuchando mensajes en la cola ${queue}`);
+  console.log(`Escuchando mensajes en la cola ${IPQueue}`);
+
+  channelIP.consume(IPQueue, async (msg) => {
+    if (msg !== null) {
+      try {
+        const msjJSON = JSON.parse(msg.content.toString());
+        IPEsp32 = msjJSON.ipLocal;
+        console.log(IPEsp32);
+
+        channelIP.ack(msg);
+      } catch (error) {
+        console.error('Error al procesar el mensaje:', error);
+        channelIP.reject(msg, false);
+      }
+    }
+  });
 
   channel.consume(queue, async (msg) => {
     if (msg !== null) {
@@ -38,7 +57,8 @@ const  url = process.env.AMQP_URL || "";
 }
 
 async function enviarMensajeALaAPI(message: any) {
-  const apiUrl = 'http://192.168.100.61/activate';
+  console.log(IPEsp32);
+  const apiUrl = `http://${IPEsp32}/activate`;
   const messageJSON = JSON.parse(message);
   console.log(messageJSON);
   const formData = `status=${encodeURIComponent(messageJSON.status)}&id=${encodeURIComponent(messageJSON.id)}&correo=${messageJSON.idPropietario}`; // Agrega los parámetros "status" e "id" a la cadena de consulta
@@ -55,4 +75,5 @@ async function enviarMensajeALaAPI(message: any) {
   }
 }
 
-consumeMessages().catch(console.error);
+consumeMessages().catch(console.error)
+
